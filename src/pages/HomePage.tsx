5a0f61d18e0237b352e11db2a9505b85b5f126ec
Sehan Alfarisi <106533701+VXerys@users.noreply.github.com>
2025-09-11 08:24:42 +0700

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,7 +9,7 @@ import QuickInfoGrid from '../components/Home/QuickInfoGrid';
 import FeaturedUMKM from '../components/Home/FeaturedUMKM';
 import StatsRow from '../components/Home/StatsRow';
 
-import logoUrl from '../../assets/barudak.svg';
+import logoUrl from '../assets/barudak.svg';
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
